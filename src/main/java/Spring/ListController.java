e7600bf78699709cfc11e0a67ffd480a99bece34
Andrey MOU Larionov <anlarionov@gmail.com>
2019-03-09 18:16:44 +0300

    We don't need special handler for filtering. We could pass "filter" as request (GET or POST) paramter and handle it in general method

--- src/main/java/Spring/ListController.java
+++ src/main/java/Spring/ListController.java
@@ -7,14 +7,9 @@ import Spring.repos.WordListRepo;
 import Spring.repos.WordRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -33,14 +28,8 @@ public class ListController {
         return "list";
     }
 
-    @GetMapping("/word_list/{id}/add_word")
-    public String add_word(@PathVariable("id") Integer listId, Map<String, Object> model) {
-        model.put("wordList", wordListRepo.findById(listId).get());
-        return "add_word";
-    }
-
-    @PostMapping("/word_list/{id}/add_word/filter")
-    public String filter(@PathVariable("id") Integer listId, @RequestParam final String filter, Map<String, Object> model) {
+    @RequestMapping(value = "/word_list/{id}/add_word", method = {RequestMethod.GET, RequestMethod.POST})
+    public String filter(@PathVariable("id") Integer listId, @RequestParam(name = "filter", required = false) final String filter, Map<String, Object> model) {
         Iterable<Word> words;
 
         if (filter != null && !filter.isEmpty()) {
